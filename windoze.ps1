96df8f815b16a535c23093cd72701907209e7231
brckd <brycked@proton.me>
2023-06-19 19:39:39 +0200

    fix(parameters): add advanced parameters

--- windoze.ps1
+++ windoze.ps1
@@ -5,15 +5,20 @@
     Glamorous shell scripts to create your very own Windows image.
 .PARAMETER Source
     Path of the image that should be altered.
-    If not specified, will be prompted
+    If not specified, will be prompted later.
+.EXAMPLE
+    ./windoze.ps1 -S ./my/image.iso
 #>
-param(
-    [string]$Source
+Param(
+    [Parameter(Position = 0)]
+    [Alias("S")]
+    [string]
+    $Source
 )
 
 $env:WINDOZE_HIGHLIGHT ??= 12
-$env:WINDOZE_CONFIRM ??= 9
-$env:WINDOZE_REJECT ??= 10
+$env:WINDOZE_CONFIRM ??= 10
+$env:WINDOZE_REJECT ??= 9
 
 <#
 .DESCRIPTION
@@ -24,7 +29,14 @@ $env:WINDOZE_REJECT ??= 10
     The command to use.
     Defaults to Select Graphic Rendition.
 #>
-function Ansi([ushort[]]$Arguments, [char]$Command = "m") {
+function Ansi(
+    [Parameter(ValueFromPipeline, Position = 0)]
+    [ushort[]]
+    $Arguments,
+    [Alias("C")]
+    [char]
+    $Command = "m"
+) {
     return $Arguments.Length ? "$([char]27)[$($Arguments -join ";")$Command" : ""
 }
 
@@ -38,22 +50,24 @@ function Ansi([ushort[]]$Arguments, [char]$Command = "m") {
 .PARAMETER Background
     The color of the background.
 #>
-function Color([string]$Text, [ushort]$Foreground, [ushort]$Background) {
-    $Args = (
+function Color(
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+    [string]
+    $Text,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias("F")]
+    [ushort]
+    $Foreground,
+    [Parameter(ValueFromPipelineByPropertyName)]
+    [Alias("B")]
+    [ushort]
+    $Background
+) {
+    $Arguments = (
         $(if ($Foreground -is [ushort]) { 38, 5, $Foreground } else { @() }) + `
         $(if ($Foreground -is [ushort]) { 38, 5, $Foreground } else { @() })
     )
-    return $Args.Length ? "$(Ansi $Args)$Text$(Ansi 0)" : "$Text"
-}
-
-<#
-.DESCRIPTION
-    Highlight a text.
-.PARAMETER Text
-    The text to highlight.
-#>
-function Highlight([string]$Text) {
-    return Color $Text $env:WINDOZE_HIGHLIGHT
+    return $Arguments.Length ? "$(Ansi $Arguments)$Text$(Ansi 0)" : "$Text"
 }
 
 <#
@@ -64,9 +78,21 @@ function Highlight([string]$Text) {
 .PARAMETER Script
     The script to execute.
 .PARAMETER Color
-    The highlight color of the spinner
+    The highlight color of the spinner.
+    Defaults to $env:WINDOZE_HIGHLIGHT.
 #>
-function Spin([string]$Text, [scriptblock]$Script, [int]$Color = $env:WINDOZE_HIGHLIGHT) {
+function Spin(
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+    [string]
+    $Text,
+    [Parameter(Mandatory, Position = 1)]
+    [Alias("S")]
+    [scriptblock]
+    $Script,
+    [Alias("C")]
+    [ushort]
+    $Color = $env:WINDOZE_HIGHLIGHT
+) {
     $Frames = "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"
     $Frame = 0
     $Job = Start-Job -ScriptBlock $Script
@@ -74,24 +100,16 @@ function Spin([string]$Text, [scriptblock]$Script, [int]$Color = $env:WINDOZE_HI
     [console]::CursorVisible = $false
     while ($Job.State -eq "Running") {
         $Frame = ($Frame + 1) % $Frames.Length
-        Write-Host -NoNewline "`r$(Status $Text $Frames[$Frame] $Color)"
+        Write-Host -NoNewline "`r$(Status $Text $Frames[$Frame] -C $Color)"
         Start-Sleep 0.04
     }
-    Reset-Line
+    Write-Host "`r$(Confirm $Text)"
     [console]::CursorVisible = $true
     
     $Result = Receive-Job $Job
     Remove-Job -Job $Job
     if ($Job.Error) { throw $Job.Error } 
     return $Result
-}
-
-<#
-.DESCRIPTION
-    Reset the current line in the console.
-#>
-function Reset-Line {
-    Write-Host -NoNewline "`r$(" " * $host.UI.RawUI.CursorPosition.X)`r"
 }
 
 <#
@@ -103,22 +121,76 @@ function Reset-Line {
     The status to prepend.
 .PARAMETER Color
     The color to use for the status.
+    Defaults to $env:WINDOZE_HIGHLIGHT.
 #>
-function Status([string]$Text, [string]$Status, [int]$Color = $env:WINDOZE_HIGHLIGHT) {
-    return "$(Color $Status $Color) $Text"
+function Status(
+    [Parameter(Mandatory, Position = 0)]
+    [string]
+    $Text,
+    [Parameter(Mandatory, Position = 1)]
+    [Alias("S")]
+    [string]
+    $Status,
+    [Alias("C")]
+    [ushort]
+    $Color = $env:WINDOZE_HIGHLIGHT
+) {
+    return "$(Color $Status -F $Color) $Text"
 }
 
-function Confirm([string]$Text, [string]$Color = $env:WINDOZE_CONFIRM) {
-    return Status $Text "✔" $Color
+<#
+.DESCRIPTION
+    Highlight a text.
+.PARAMETER Text
+    The text to highlight.
+    Defaults to $env:WINDOZE_HIGHLIGHT.
+#>
+function Highlight([Parameter(Mandatory, Position = 0, ValueFromPipeline)][string]$Text) {
+    return Color $Text -F $env:WINDOZE_HIGHLIGHT
 }
 
-function Reject([string]$Text, [string]$Color = $env:WINDOZE_REJECT) {
-    return Status $Text "✘" $Color
+<#
+.DESCRIPTION
+    Format a confirmation.
+.PARAMETER Text
+    The confirmation text.
+.PARAMETER Color
+    The color to use for the confirmation icon.
+    Defaults to $env:WINDOZE_CONFIRM.
+#>
+function Confirm(
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+    [string]
+    $Text,
+    [Alias("C")]
+    [ushort]
+    $Color = $env:WINDOZE_CONFIRM
+) {
+    return Status $Text "✔" -C $Color
+}
+
+<#
+.DESCRIPTION
+    Format a rejection.
+.PARAMETER Text
+    The rejection text.
+.PARAMETER Color
+    The color to use for the rejection icon.
+    Defaults to $env:WINDOZE_REJECT.
+#>
+function Reject(
+    [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+    [string]
+    $Text,
+    [Alias("C")]
+    [ushort]
+    $Color = $env:WINDOZE_REJECT
+) {
+    return Status $Text "✘" -C $Color
 }
 
 # Print welcome screen.
 Write-Output "`nWelcome to $(Highlight "Windoze") image creator!`n"
-
 Spin "Processing..." { Start-Sleep 3 }
 Confirm "Done"
 Reject "Error"
